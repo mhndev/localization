48579d130dcc085e49cb3041f2fd98bc1ab334df
Majid Abdolhosseini <majid8911303@gmail.com>
2017-04-23 10:45:20 +0430

    code cleanup and write more tests

--- tests/TranslateTest.php
+++ tests/TranslateTest.php
@@ -1,7 +1,8 @@
 <?php
 namespace mhndev\localization\Tests;
 
-use mhndev\localization\LanguageEnglish;
+use mhndev\localization\interfaces\iLanguage;
+use mhndev\localization\LanguageFactory;
 use mhndev\localization\SourcePhpArray;
 use mhndev\localization\Translator;
 use org\bovigo\vfs\vfsStream;
@@ -33,19 +34,60 @@ class TranslateTest extends TestCase
 
         $path = vfsStream::url("rootDirectory").DIRECTORY_SEPARATOR."en.php";
 
-        file_put_contents($path , '<?php return  ' . var_export($translationArray, true) . ';');
+        file_put_contents(
+            $path ,
+            '<?php return  ' . var_export($translationArray, true) . ';'
+        );
 
         $source = new SourcePhpArray($path);
-        $lang_en = (new LanguageEnglish())->setSource($source);
+        $lang_en = LanguageFactory::fromUrlCode('en')->setSource($source);
 
         $translator = new Translator();
         $translator->addLanguage($lang_en);
 
-        $result = $translator->translate('greet', 'en', ['name' => 'majid', 'age' => 20] );
+        $result = $translator->translate(
+            'greet',
+            'en',
+            ['name' => 'majid', 'age' => 20]
+        );
 
 
 
         $this->assertEquals('hello this is majid and I am 20 years old.', $result);
+    }
+
+
+    function testLanguageFactory()
+    {
+        $language1 = LanguageFactory::fromUrlCode('en');
+
+        $language2 = LanguageFactory::fromCountryCode('us');
+
+        $language3 = LanguageFactory::fromCalender('gregorian');
+
+        $language4 = LanguageFactory::fromLocale('en_US');
+
+        $language5 = LanguageFactory::fromName('english');
+
+        $farsi = LanguageFactory::fromLocale('fa_IR');
+
+        $this->assertInstanceOf(iLanguage::class, $language1);
+        $this->assertInstanceOf(iLanguage::class, $language2);
+        $this->assertInstanceOf(iLanguage::class, $language3);
+        $this->assertInstanceOf(iLanguage::class, $language4);
+        $this->assertInstanceOf(iLanguage::class, $language5);
+
+        $this->assertEquals('english', $language1->getName());
+        $this->assertEquals('en_US', $language1->getLocale());
+        $this->assertEquals('en', $language1->getUrlCode());
+        $this->assertEquals('gregorian', $language1->getCalendar());
+        $this->assertEquals('us', $language1->getCountryCode());
+
+        $this->assertEquals('farsi', $farsi->getName());
+        $this->assertEquals('fa_IR', $farsi->getLocale());
+        $this->assertEquals('fa', $farsi->getUrlCode());
+        $this->assertEquals('persian', $farsi->getCalendar());
+        $this->assertEquals('ir', $farsi->getCountryCode());
     }
 
 }
